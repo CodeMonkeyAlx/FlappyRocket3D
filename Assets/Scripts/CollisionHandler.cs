ae1488bbf93c5d1c141ef54d32928ab5df727280
Alex Kollar <codemonkeyalx@gmail.com>
2021-02-12 12:25:06 -0500

    Rocket Man
    - Rocket Functions as it should.
    - Added collision detection
    - Basic audio on rocket thrust
    - Code Tidied up
    
    - Removed Rogue Walla's

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) 
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly!");
+                break;
+            case "Finish":
+                Debug.Log("Congrats you finished the level!");
+                break;
+            default:
+                Debug.Log("Sorry you blew up!");
+                ReloadLevel();
+                break;
+        }
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,62 @@
-﻿using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    AudioSource audio;
+    [SerializeField] float thrustForce = 10f;
+    [SerializeField] float RotationThrust = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Cache refference to rigid body varible.
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
+            if(!audio.isPlaying)
+            {
+                audio.Play();
+            }
+        }
+        else
+        {
+            audio.Stop();
+        }
+    }
+
+    void ProcessRotation() 
+    {
+       if(Input.GetKey(KeyCode.A))
+        {
+            //rotate rocket left
+            ApplyRotation(RotationThrust);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            //Rotate Rocket Right.
+            ApplyRotation(-RotationThrust);
+        }    
+    }
+
+    void ApplyRotation(float rotationThisFrame) 
+    {
+        rb.freezeRotation = true; //Freeze Rotation so we can manually control the rocket.
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; //Unfreeze constraints. 
     }
 }
